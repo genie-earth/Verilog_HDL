992d983e14451a658a8e7b8baf47c8986504ff43
genie-earth <83503959+genie-earth@users.noreply.github.com>
2021-09-08 21:36:11 +0900

    Update tb_d_ff.v

--- 2_D_flip_flop/tb_d_ff.v
+++ 2_D_flip_flop/tb_d_ff.v
@@ -16,25 +16,25 @@ always
  
 initial begin
     clk     		= 0;
-	clk_enable		= 0; // no input clk
-	sync_reset		= 0;
-	async_reset		= 0;
-	async_reset_n 	= 1;
-	i_value			= 1;
+    clk_enable		= 0; // no input clk
+    sync_reset		= 0;
+    async_reset		= 0;
+    async_reset_n 	= 1;
+    i_value			= 1;
 #50 // Async reset
-	sync_reset		= 1;
-	async_reset		= 1;
-	async_reset_n 	= 0;
+    sync_reset		= 1;
+    async_reset		= 1;
+    async_reset_n 	= 0;
 #10
-	sync_reset		= 0;
-	async_reset		= 0;
-	async_reset_n 	= 1;
+    sync_reset		= 0;
+    async_reset		= 0;
+    async_reset_n 	= 1;
 #10 // Input clock
-	clk_enable		= 1; // input clk
+    clk_enable		= 1; // input clk
 #10 // Sync reset
-	sync_reset		= 1;
+    sync_reset		= 1;
 #10
-	sync_reset		= 0;
+    sync_reset		= 0;
 #50
 $finish;
 end
@@ -42,19 +42,16 @@ end
 wire clk_for_dut = clk && clk_enable;
 
 d_ff_test DUT(
-    .clk						(clk_for_dut  ),
-    .sync_reset					(sync_reset	  ),
-    .async_reset				(async_reset  ),
-    .async_reset_n				(async_reset_n),
-
-	.i_value					(i_value	  ),
-    .o_value_sync_reset			(),
-    .o_value_async_reset		(),
-    .o_value_async_reset_n		(),
-    .o_value_mixed_reset		(),
-    .o_value_no_reset			() 
+    .clk			(clk_for_dut),
+    .sync_reset			(sync_reset),
+    .async_reset		(async_reset),
+    .async_reset_n		(async_reset_n),
+    .i_value			(i_value),
+    .o_value_sync_reset		(),
+    .o_value_async_reset	(),
+    .o_value_async_reset_n	(),
+    .o_value_mixed_reset	(),
+    .o_value_no_reset		() 
 );
-
-
 
 endmodule
