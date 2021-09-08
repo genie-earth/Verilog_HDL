b2b5161be9fe8b116fdfed3efca58f4af26cbfda
genie-earth <83503959+genie-earth@users.noreply.github.com>
2021-09-08 21:41:27 +0900

    Update clock_gating_model.v

--- 1_clock_gating_model/clock_gating_model.v
+++ 1_clock_gating_model/clock_gating_model.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 module clock_gating_model(
-    input 	i_clk,	        // 100MHz clock
-    input 	i_clock_en, 	// clock providing during HIGH
-    output  o_clk);
+    input       i_clk,	        // 100MHz clock
+    input       i_clock_en, 	// clock providing during HIGH
+    output      o_clk);
 
     assign o_clk = i_clk & i_clock_en;	// clock AND gating
 
