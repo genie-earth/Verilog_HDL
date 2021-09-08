5c69f354d9d24b8917c389696f30f92451c4b8d9
genie-earth <83503959+genie-earth@users.noreply.github.com>
2021-09-08 23:08:33 +0900

    Update counter_100.v

--- 4_counter/counter_100.v
+++ 4_counter/counter_100.v
@@ -1,31 +1,31 @@
 `timescale 1ns / 1ps
 
 module counter_100(
-    input clk,
-    input reset_n,
-    output [6:0] o_cnt,
-    output [6:0] o_cnt_always	);
+    input 		clk,
+    input 		reset_n,
+    output [6:0] 	o_cnt,
+    output [6:0] 	o_cnt_always	);
 	
-    reg [6:0] cnt;							// To avoid overflow (range: 0~127)
+    reg [6:0] cnt;					// To avoid overflow (range: 0~127)
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n) begin
-            cnt <= 0;  						// Counting start from zero
+            cnt <= 0;  					// Counting start from zero
         end else if (cnt < 100) begin
-            cnt <= cnt + 1;					// Increment whenever clock is postive edge
+            cnt <= cnt + 1;				// Increment whenever clock is postive edge
         end
         // Counting stops when cnt exceeds 100
     end
 	
     assign o_cnt = cnt;
 
-    reg [6:0] cnt_always;					// To avoid overflow (range: 0~127) (2^7)
+    reg [6:0] cnt_always;				// To avoid overflow (range: 0~127) (2^7)
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n) begin
             cnt_always <= 0;  				// Counting start from zero
         end else if (cnt_always == 100-1) begin
             cnt_always <= 0; 				// 99 -> 0
         end else begin
-            cnt_always <= cnt_always + 1;	// Increment whenever clock is postive edge
+            cnt_always <= cnt_always + 1;		// Increment whenever clock is postive edge
         end
     end
 	
