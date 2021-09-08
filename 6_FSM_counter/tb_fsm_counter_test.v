64799684e0f8669a2df4ea75396e75914010b782
genie-earth <83503959+genie-earth@users.noreply.github.com>
2021-09-09 00:28:51 +0900

    Update tb_fsm_counter_test.v

--- 6_FSM_counter/tb_fsm_counter_test.v
+++ 6_FSM_counter/tb_fsm_counter_test.v
@@ -2,11 +2,11 @@
 
 module tb_fsm_counter_test;
 reg clk , reset_n;
-reg 			i_run;
-reg  [6:0]		i_num_cnt;
-wire 			o_idle;
-wire 			o_running;
-wire 			o_done;
+reg 		i_run;
+reg  [6:0]	i_num_cnt;
+wire 		o_idle;
+wire 		o_running;
+wire 		o_done;
  
 // clk gen
 always
@@ -17,8 +17,8 @@ initial begin
 $display("initialize value [%d]", $time);
     reset_n = 1;
     clk     = 0;
-	i_run 	= 0;
-	i_num_cnt = 0;
+    i_run 	= 0;
+    i_num_cnt = 0;
 	
 // reset_n gen
 $display("Reset! [%d]", $time);
@@ -27,34 +27,26 @@ $display("Reset! [%d]", $time);
 # 10
     reset_n = 1;
 # 10
-@(posedge clk);
-
-
-$display("Check Idle [%d]", $time);
-wait(o_idle);
-
-$display("Start! [%d]", $time);
-	i_num_cnt = 7'd100;
-	i_run = 1;
-@(posedge clk);
-	i_run = 0;
-
-$display("Wait Done [%d]", $time);
-wait(o_done);
-
+    @(posedge clk);
+    wait(o_idle);
+    i_num_cnt = 7'd100;
+    i_run = 1;
+    @(posedge clk);
+    i_run = 0;
+    wait(o_done);
 # 100
-$display("Finish! [%d]", $time);
-$finish;
+    $finish;
 end
 
 // Call DUT
 fsm_counter_test u_fsm_counter_test(
-    .clk 			(clk),
-    .reset_n 		(reset_n),
-	.i_run 			(i_run),
-	.i_num_cnt 		(i_num_cnt),
-	.o_idle 		(o_idle),
-	.o_running 		(o_running),
-	.o_done 		(o_done)
+    .clk		(clk),
+    .reset_n		(reset_n),
+    .i_run		(i_run),
+    .i_num_cnt 		(i_num_cnt),
+    .o_idle 		(o_idle),
+    .o_running 		(o_running),
+    .o_done 		(o_done)
     );
+
 endmodule
