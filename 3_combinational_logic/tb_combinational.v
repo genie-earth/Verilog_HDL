eb4ac733c8faa45b5f8bf90c32f5c1b670ed1f9c
genie-earth <83503959+genie-earth@users.noreply.github.com>
2021-09-08 22:52:02 +0900

    Update tb_combinational.v

--- 3_combinational_logic/tb_combinational.v
+++ 3_combinational_logic/tb_combinational.v
@@ -7,15 +7,10 @@ reg clk;	// no use
 reg [7:0] value_a;
 reg [7:0] value_b;
 
-wire [7:0] w_value_add_0;
-wire [7:0] w_value_sub_0;
-wire [7:0] w_value_mul_0;
-wire [7:0] w_value_div_0;
-
-wire [7:0] w_value_add_1;
-wire [7:0] w_value_sub_1;
-wire [7:0] w_value_mul_1;
-wire [7:0] w_value_div_1;
+wire [7:0] w_value_add;
+wire [7:0] w_value_sub;
+wire [7:0] w_value_mul;
+wire [7:0] w_value_div;
  
 initial begin
 #10
@@ -25,22 +20,13 @@ initial begin
 $finish;
 end
 
-combinational_0 DUT_0(
+combinational DUT(
     .i_value_a		(value_a),
     .i_value_b		(value_b),
-    .o_value_add	(w_value_add_0),
-    .o_value_sub	(w_value_sub_0),
-    .o_value_mul	(w_value_mul_0),
-    .o_value_div	(w_value_div_0)
-);
-
-combinational_1 DUT_1(
-    .i_value_a		(value_a),
-    .i_value_b		(value_b),
-    .o_value_add	(w_value_add_1),
-    .o_value_sub	(w_value_sub_1),
-    .o_value_mul	(w_value_mul_1),
-    .o_value_div	(w_value_div_1)
+    .o_value_add	(w_value_add),
+    .o_value_sub	(w_value_sub),
+    .o_value_mul	(w_value_mul),
+    .o_value_div	(w_value_div)
 );
 
 endmodule
