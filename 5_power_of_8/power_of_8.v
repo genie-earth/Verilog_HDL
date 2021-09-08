7565b26071c7b3bfec4bb427a9526fcefb0aef1c
genie-earth <83503959+genie-earth@users.noreply.github.com>
2021-09-08 23:55:25 +0900

    Update power_of_8.v

--- 5_power_of_8/power_of_8.v
+++ 5_power_of_8/power_of_8.v
@@ -21,25 +21,25 @@ module power_of_8(
         if(!reset_n) begin
             r_valid <= 3'd0;
         end else begin
-            r_valid <= {r_valid[1:0], i_valid};
+            r_valid <= {r_valid[1:0], i_valid};		// concatenation of previous valid signal with current valid signal (shift-left)
         end
     end
 
 // data buffer (f/f) 
-    always @(posedge clk or negedge reset_n) begin
+    always @(posedge clk or negedge reset_n) begin	// connecting data buffer to flip-flop
         if(!reset_n) begin
             r_power_of_2 <= 64'd0;
             r_power_of_4 <= 64'd0;
             r_power_of_8 <= 64'd0;
         end else begin
-            r_power_of_2 <= power_of_2;
+	    r_power_of_2 <= power_of_2;			// first output of multiplier (power_of_2) goes to second input of F/F (r_power_of_2)
             r_power_of_4 <= power_of_4;
             r_power_of_8 <= power_of_8;
         end
     end
 
 // Power Operation
-    assign power_of_2 = i_value * i_value;
+    assign power_of_2 = i_value * i_value;		// first output of F/F (i_value) is multplied itself to be first output of multiplier (power_of_2)
     assign power_of_4 = r_power_of_2 * r_power_of_2;
     assign power_of_8 = r_power_of_4 * r_power_of_4;
 	
