2f7dd88109591c2cc60bb32312ba29c374045c97
genie-earth <83503959+genie-earth@users.noreply.github.com>
2021-09-09 00:03:00 +0900

    Update power_of_8.v

--- 5_power_of_8/power_of_8.v
+++ 5_power_of_8/power_of_8.v
@@ -32,18 +32,18 @@ module power_of_8(
             r_power_of_4 <= 64'd0;
             r_power_of_8 <= 64'd0;
         end else begin
-	    r_power_of_2 <= power_of_2;			// first output of multiplier (power_of_2) goes to second input of F/F (r_power_of_2)
-            r_power_of_4 <= power_of_4;
-            r_power_of_8 <= power_of_8;
+            r_power_of_2 <= power_of_2;			// first output of multiplier (power_of_2) goes to first input of F/F (r_power_of_2)
+            r_power_of_4 <= power_of_4;			// second output of multiplier (power_of_4) goes to second input of F/F (r_power_of_4)
+            r_power_of_8 <= power_of_8;			// third output of multiplier (power_of_8) goes to third input of F/F (r_power_of_8)
         end
     end
 
 // Power Operation
-    assign power_of_2 = i_value * i_value;		// first output of F/F (i_value) is multplied itself to be first output of multiplier (power_of_2)
-    assign power_of_4 = r_power_of_2 * r_power_of_2;
-    assign power_of_8 = r_power_of_4 * r_power_of_4;
+    assign power_of_2 = i_value * i_value;		// initial input (i_value) is multplied itself to be first output of multiplier (power_of_2)
+    assign power_of_4 = r_power_of_2 * r_power_of_2;	// first output of F/F (r_power_of_2) is multplied itself to be second output of multiplier (power_of_4)
+    assign power_of_8 = r_power_of_4 * r_power_of_4;	// second output of F/F (r_power_of_4) is multplied itself to be third output of multiplier (power_of_8)
 	
     assign o_valid = r_valid[2];
-    assign o_power_of_8 = r_power_of_8;
+    assign o_power_of_8 = r_power_of_8;			// third output of FF (r_power_of_8) goes to final output (o_power_of_8)
 
 endmodule
